69d0c0f3cc588a1fe7d78d823313e0c465e96a87
Victor Geere <victorgeere.public@digitalpro.co.za>
2013-07-09 11:57:03 +0200

    authenticated routes

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,14 +1,17 @@
 <?php
 
-Route::get('/db', function() {
+//create a named route
+//Route::get('/db/'.$table, array('as' => 'db.'.$table, 'uses' => 'DbController@getIndex'));
+
+Route::get('/db', array('before' => 'auth.basic', function() {
     echo "<a href=\"/db/api/asdf\">Greeting</a><br />";
     echo "<a href=\"/db/table\">Table</a><br />";
-});
+}));
 
-Route::get('/db/api/{call}', function() {
+Route::get('/db/table/{table}', array('before' => 'auth.basic', 'uses' => 'DbController@getIndex'));
+
+Route::get('/db/api/{call}', array('before' => 'auth.basic', function() {
     echo DbGopher::greeting();
-});
-
-Route::get('/db/{table}', 'DbController@getIndex');
+}));
 
 ?>
