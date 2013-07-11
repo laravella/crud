41ea6057c94b8d760882fcfdd27d869fdac086ad
victorgeere <wrigglingsnake@gmail.com>
2013-07-11 15:14:15 +0200

    test routes

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,27 +1,41 @@
 <?php
 
 /*
-//create a named route
-Route::get('/db/'.$table, array('as' => 'db.'.$table, 'uses' => 'DbController@getIndex'));
-*/
+  //create a named route
+  Route::get('/db/'.$table, array('as' => 'db.'.$table, 'uses' => 'DbController@getIndex'));
+ */
 
-Route::get('/db', array('before' => 'auth.basic', function() {
-    echo "<a href=\"/db/api/asdf\">Greeting</a><br />";
-    echo "<a href=\"/db/table\">Table</a><br />";
-}));
 
-Route::get('/db/table', array('before' => 'auth.basic', function() {
-    echo "Use : /db/table/&lt;tablename&gt;";
-}));
+Route::get('/asdf', function()
+        {
+            echo "asdf";
+        });
 
-Route::get('/db/api', array('before' => 'auth.basic', function() {
-    echo "Use : /db/api/&lt;apicall&gt;";
-}));
+Route::filter('crudauth', function()
+        {
+            
+        });
+
+Route::get('/db', array('before' => 'auth.basic', function()
+    {
+        echo "<a href=\"/db/api/asdf\">Greeting</a><br />";
+        echo "<a href=\"/db/table\">Table</a><br />";
+    }));
+
+Route::get('/db/table', array('before' => 'auth.basic', function()
+    {
+        echo "Use : /db/table/&lt;tablename&gt;";
+    }));
+
+Route::get('/db/api', array('before' => 'auth.basic', function()
+    {
+        echo "Use : /db/api/&lt;apicall&gt;";
+    }));
 
 Route::get('/db/table/{table}', array('before' => 'auth.basic', 'uses' => 'DbController@getIndex'));
 
-Route::get('/db/api/{call}', array('before' => 'auth.basic', function() {
-    echo DbGopher::greeting();
-}));
-
+Route::get('/db/api/{call}', array('before' => 'auth.basic', function()
+    {
+        echo DbGopher::greeting();
+    }));
 ?>
