b4f1ba7160c67943e8fb6200190c75bc4ceaa614
victorgeere <wrigglingsnake@gmail.com>
2013-07-11 16:25:07 +0200

    DbController

--- src/routes.php
+++ src/routes.php
@@ -1,41 +1,5 @@
 <?php
 
-/*
-  //create a named route
-  Route::get('/db/'.$table, array('as' => 'db.'.$table, 'uses' => 'DbController@getIndex'));
- */
-
-
 Route::get('/asdf', function() { return "asdf";});
 
-        /*
-Route::filter('crudauth', function()
-        {
-            
-        });
-
-Route::get('/db', array('before' => 'auth.basic', function()
-    {
-        echo "<a href=\"/db/api/asdf\">Greeting</a><br />";
-        echo "<a href=\"/db/table\">Table</a><br />";
-    }));
-
-Route::get('/db/table', array('before' => 'auth.basic', function()
-    {
-        echo "Use : /db/table/&lt;tablename&gt;";
-    }));
-
-Route::get('/db/api', array('before' => 'auth.basic', function()
-    {
-        echo "Use : /db/api/&lt;apicall&gt;";
-    }));
-
-Route::get('/db/table/{table}', array('before' => 'auth.basic', 'uses' => 'DbController@getIndex'));
-
-Route::get('/db/api/{call}', array('before' => 'auth.basic', function()
-    {
-        echo DbGopher::greeting();
-    }));
-         * 
-         */
 ?>
