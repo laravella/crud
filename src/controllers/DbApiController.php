f77a7595877f78028e6d96e963aace353cf19f0b
laravella <laravella.cms@gmail.com>
2013-07-24 11:08:14 +0200

    dbapiController

---
+++ src/controllers/DbApiController.php
@@ -0,0 +1,5 @@
+<?php class DbApiController extends Controller {
+    
+}
+
+?>
--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::controller('db', 'DbController');
+Route::controller('dbapi', 'DbApiController');
 Route::controller('dbinstall', 'DbInstallController');
 
 ?>
