a50aa45b855ecfc8f0e9487d15b8039f15fbf5eb
laravella <laravella.cms@gmail.com>
2013-08-12 10:10:25 +0200

    restore seeder

---
+++ src/database/seeds/CrudRestoreSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Laravella\Crud\Log;
+
+class CrudRestoreSeeder extends Seeder {
+
+    public function run()
+    {
+        if (Schema::hasTable('_db_backups'))
+
+        }
+
+        if (Schema::hasTable('_db_bak_menus'))
+        {
+        }
+
+        if (Schema::hasTable('_db_bak_permissions'))
+        {
+        }
+    }
+
+}
