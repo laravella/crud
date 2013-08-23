f88515f7e4b9dda8ecd688960140200b12ca519c
laravella <laravella.cms@gmail.com>
2013-08-23 11:06:02 +0200

    options in thumbnail path

--- src/Laravella/Crud/Options.php
+++ src/Laravella/Crud/Options.php
@@ -7,6 +7,14 @@
  */
 class Options {
 
+    public function get($name) {
+        $setting = '';
+        $option = DB::table('_db_options')->where('name', $name)-first();
+        if (is_object($option)) {
+            $setting = $option->value;
+        }
+        return $setting;
+    }
 
 }
 
