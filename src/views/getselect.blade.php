f5f3131fe9c44ee7b817c22ba66fff9ed923b8e6
laravella <laravella.cms@gmail.com>
2013-09-12 15:01:04 +0200

    make more fault tollerant

--- src/views/getselect.blade.php
+++ src/views/getselect.blade.php
@@ -86,7 +86,7 @@
                 || ($displayTypes[$meta[$name]['display_type_id']] == 'widget' 
                 && $widgetTypes[$meta[$name]['widget_type_id']] == 'thumbnail'))
                     <td>
-                        @if(file_exists(public_path().'/uploads/thumbnail/'.$record->file_name))
+                        @if(is_object($record) && property_exists($record, 'file_name') && file_exists(public_path().'/uploads/thumbnail/'.$record->file_name))
                             <a href="{{$prefix[$name]}}{{$value}}"><img src="/uploads/thumbnail/{{$record->file_name}}" class="img-rounded" style="width:80px; height:80px; max-width:80px" /></a>
                         @else
                             <i class="icon-file"> </i>
