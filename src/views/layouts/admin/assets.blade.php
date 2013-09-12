130dddeb2815e6cc601ed7ed7b83516cbf79ab7a
laravella <laravella.cms@gmail.com>
2013-09-12 11:25:06 +0200

    fix assets

--- src/views/layouts/admin/assets.blade.php
+++ src/views/layouts/admin/assets.blade.php
@@ -1,23 +1,23 @@
 @section('assets_css')
 <!-- CSS -->
 
-<link rel="stylesheet" href="assets/styles/css/main.css">
-<link rel="stylesheet" href="packages/laravella/crud/assets/styles/adminstyles.css">
+<link rel="stylesheet" href="/assets/styles/css/main.css">
+<link rel="stylesheet" href="/packages/laravella/crud/assets/styles/adminstyles.css">
 @stop
 
 @section('assets_js')		
 <!-- JS -->
 
 <!-- jQuery -->
-<script type="text/javascript" src="packages/laravella/crud/assets/scripts/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="/packages/laravella/crud/assets/scripts/jquery-1.8.3.min.js"></script>
 
 <!-- JS -->
-<script type="text/javascript" src="packages/laravella/crud/assets/scripts/modernizr-2.6.2.min.js"></script>
-<script type="text/javascript" src="packages/laravella/crud/assets/scripts/jsonconvert.js"></script>
-<script type="text/javascript" src="packages/laravella/crud/assets/scripts/admintools.js"></script>
+<script type="text/javascript" src="/packages/laravella/crud/assets/scripts/modernizr-2.6.2.min.js"></script>
+<script type="text/javascript" src="/packages/laravella/crud/assets/scripts/jsonconvert.js"></script>
+<script type="text/javascript" src="/packages/laravella/crud/assets/scripts/admintools.js"></script>
 
 <!-- CKEditor -->
-<script type="text/javascript" src="packages/laravella/crud/assets/scripts/ckeditor/ckeditor.js"></script>
+<script type="text/javascript" src="/packages/laravella/crud/assets/scripts/ckeditor/ckeditor.js"></script>
 
 <!-- Internet Explorer .png-fix -->
 
--- src/views/layouts/default.blade.php
+++ src/views/layouts/default.blade.php
@@ -83,15 +83,9 @@
 
         @yield('bottom')
         
-        <!-- jQuery -->
-        <!--
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="assets/scripts/js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
-        -->
-        
-        <script src="{{ asset('assets/scripts/js/plugins.js') }}"></script>
-        <script src="{{ asset('assets/scripts/js/main.js') }}"></script>
-        <script src="{{ asset('assets/scripts/js/vendor/bootstrap.min.js') }}"></script>
+        <script src="assets/scripts/js/plugins.js"></script>
+        <script src="assets/scripts/js/main.js"></script>
+        <script src="assets/scripts/js/vendor/bootstrap.min.js"></script>
         
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
