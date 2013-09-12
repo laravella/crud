132460e9396543c6a9d68ec65c8758f983339bb6
laravella <laravella.cms@gmail.com>
2013-09-12 11:04:40 +0200

    fix asset references

--- src/views/layouts/admin/assets.blade.php
+++ src/views/layouts/admin/assets.blade.php
@@ -1,47 +1,30 @@
 @section('assets_css')
-        <!-- CSS asdfasdf-->
+<!-- CSS -->
 
-                <link rel="stylesheet" href="{{ asset('assets/styles/css/main.css')}} ">
-                <link rel="stylesheet" href="/packages/laravella/crud/assets/styles/adminstyles.css">
-	  
-		<!-- icons css style -->
-		<link rel="stylesheet" href="<?php echo admin_asset('admin/fontawesome/css/font-awesome.min.css');?>" type="text/css" media="screen" />
-	  
-		<!-- Internet Explorer Fixes Stylesheet -->
-		
-		<!--[if lte IE 7]>
-			
-			<link rel="stylesheet" href="<?php echo admin_asset('admin/fontawesome/css/font-awesome-ie7.min.css');?>" type="text/css" media="screen" />
-	  
-			<link rel="stylesheet" href="<?php echo admin_asset('admin/css/ie.css');?>" type="text/css" media="screen" />
-		<![endif]-->
-
-		<!-- jquery ui -->
-		<link rel="stylesheet" href="<?php echo admin_asset('admin/css/smoothness/jquery-ui-1.10.0.custom.min.css');?>" type="text/css" media="screen" />
-		
+<link rel="stylesheet" href="assets/styles/css/main.css">
+<link rel="stylesheet" href="packages/laravella/crud/assets/styles/adminstyles.css">
 @stop
 
 @section('assets_js')		
-                <!-- JS -->
-  
-		<!-- jQuery -->
-		<script type="text/javascript" src="<?php echo admin_asset('admin/scripts/jquery.min.js');?>"></script>
-		<script type="text/javascript" src="<?php echo admin_asset('admin/scripts/jquery-ui-1.10.0.smoothness.min.js');?>"></script>
-                
-                <!-- JS -->
-                <script src="{{ asset('assets/scripts/js/vendor/modernizr-2.6.2.min.js') }}"></script>
-                <script type="text/javascript" src="/packages/laravella/crud/assets/scripts/jsonconvert.js"></script>
-                <script type="text/javascript" src="/packages/laravella/crud/assets/scripts/admintools.js"></script>
-                
-		<!-- CKEditor -->
-		<script type="text/javascript" src="<?php echo admin_asset('admin/scripts/ckeditor/ckeditor.js');?>"></script>
-                
-		<!-- Internet Explorer .png-fix -->
-		
-		<!--[if IE 6]>
-			<script type="text/javascript" src="<?php echo admin_asset('admin/scripts/DD_belatedPNG_0.0.7a.js');?>"></script>
-			<script type="text/javascript">
-				DD_belatedPNG.fix('.png_bg, img, li');
-			</script>
-		<![endif]-->
+<!-- JS -->
+
+<!-- jQuery -->
+<script type="text/javascript" src="packages/laravella/crud/assets/scripts/jquery-1.8.3.min.js"></script>
+
+<!-- JS -->
+<script type="text/javascript" src="packages/laravella/crud/assets/scripts/modernizr-2.6.2.min.js"></script>
+<script type="text/javascript" src="packages/laravella/crud/assets/scripts/jsonconvert.js"></script>
+<script type="text/javascript" src="packages/laravella/crud/assets/scripts/admintools.js"></script>
+
+<!-- CKEditor -->
+<script type="text/javascript" src="packages/laravella/crud/assets/scripts/ckeditor/ckeditor.js"></script>
+
+<!-- Internet Explorer .png-fix -->
+
+<!--[if IE 6]>
+        <script type="text/javascript" src="packages/laravella/crud/assets/scripts/DD_belatedPNG_0.0.7a.js"></script>
+        <script type="text/javascript">
+                DD_belatedPNG.fix('.png_bg, img, li');
+        </script>
+<![endif]-->
 @stop
