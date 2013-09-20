12e2e740a663db2349b9149388d6c7b46d7cc388
laravella <laravella.cms@gmail.com>
2013-09-20 11:15:56 +0200

    list front end route

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,15 @@
 <?php
 
+Route::filter('frontfilter', function($route)
+{
+    if (Request::segment(1) == 'en' && Request::segment(2) == 'list')
+    {
+        
+    } else {
+        return Redirect::to('/account/login');
+    }
+});
+
 Route::filter('crudauth', function()
         {
             if (Request::ajax())
@@ -20,10 +30,12 @@ Route::get('admin', function()
         });
         
 Route::when('db/*', 'crudauth');
+Route::when('en/*', 'frontfilter'); //guest use i.e. front end
 Route::when('dbapi/*', 'crudauth');
 //Route::when('dbinstall/*', 'crudauth');
 
 Route::controller('db', 'DbController');
+Route::controller('en', 'DbController');
 Route::controller('dbapi', 'DbApiController');
 //Route::controller('dbinstall', 'DbInstallController');
 
