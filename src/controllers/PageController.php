195f673f8beef94e37f8489da53719add6d21b34
dataphile <victor@digitalpro.co.za>
2013-10-31 09:45:54 +0200

    fix pageController declaration

--- src/controllers/PageController.php
+++ src/controllers/PageController.php
@@ -4,7 +4,7 @@
  * This is used for ajax calls. 
  * It just formats the data differently but the work is still done by DbController class.
  */
-class DbApiController extends DbController {
+class PageController extends DbController {
     public $displayType = self::HTML; //or self::JSON or self::HTML
     
     
